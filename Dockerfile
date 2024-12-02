7b94461192dfcc64ea783dfdc9addeef537b9163
notzeldon <spanferov@pulsar-msk.ru>
2024-12-02 05:02:55 +0300

    found and fixed problem

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ COPY requirements.txt .
 RUN pip install -r requirements.txt
 
 COPY main.py .
-CMD python3 main.py consume --topic hello_topic --kafka kafka:9021
+CMD bash -c "python3 main.py init --topic hello_topic --kafka kafka:9093 && python3 main.py consume --topic hello_topic --kafka kafka:9093"
--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import argparse
-import logging
-import sys
-
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 from confluent_kafka import Producer, Consumer, KafkaException
+from confluent_kafka.admin import AdminClient, NewTopic
 
 parser = argparse.ArgumentParser(description="Apache Kafka test app")
 subparsers = parser.add_subparsers(dest='command')
@@ -21,6 +17,11 @@ parser_produce = subparsers.add_parser('consume')
 parser_produce.add_argument('--topic', type=str)
 parser_produce.add_argument('--kafka', type=str)
 
+# init --topic 'hello_topic' --kafka 'ip:port
+parser_produce = subparsers.add_parser('init')
+parser_produce.add_argument('--topic', type=str)
+parser_produce.add_argument('--kafka', type=str)
+
 
 def delivery_report(err, msg):
     if err is not None:
@@ -32,8 +33,6 @@ def delivery_report(err, msg):
 def run_produce(message: str, topic: str, kafka: str):
     config = {
         'bootstrap.servers': kafka,
-        'group.id': 'test',
-        'auto.offset.reset': 'earliest',
         'security.protocol': 'PLAINTEXT'  # 'SSL' или 'PLAINTEXT'
     }
     producer = Producer(config)
@@ -42,7 +41,6 @@ def run_produce(message: str, topic: str, kafka: str):
 
 
 def run_consume(topic: str, kafka: str):
-    logging.info(kafka)
     config = {
         'bootstrap.servers': kafka,
         'group.id': 'test',
@@ -67,12 +65,44 @@ def run_consume(topic: str, kafka: str):
         consumer.close()  # не забываем закрыть соединение
 
 
+def run_init(topic: str, kafka: str):
+    config = {
+        'bootstrap.servers': kafka,
+    }
+    admin_client = AdminClient(config)
+
+    new_topics = [NewTopic(topic, num_partitions=1, replication_factor=1)]
+    try:
+        fs = admin_client.create_topics(new_topics)
+
+        # Ожидание завершения создания тем.
+        for topic, future in fs.items():
+            try:
+                future.result()  # Блокирует выполнение до завершения
+                print(f"Тема '{topic}' успешно создана.")
+            except Exception as e:
+                print(f"Ошибка при создании темы '{topic}': {e}")
+                return False  # Вернуть False в случае ошибки
+
+        return True  # Вернуть True, если все темы созданы успешно
+
+    except Exception as e:
+        print(f"Общая ошибка: {e}")
+        return False
+
+    finally:
+        pass
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     args = parser.parse_args()
     if args.command == 'produce':
         run_produce(message=args.message, topic=args.topic, kafka=args.kafka)
     elif args.command == 'consume':
+        run_init(topic=args.topic, kafka=args.kafka)
         run_consume(topic=args.topic, kafka=args.kafka)
+    elif args.command == 'init':
+        run_init(topic=args.topic, kafka=args.kafka)
     else:
         parser.print_help()
